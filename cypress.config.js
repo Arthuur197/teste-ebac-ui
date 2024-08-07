1829aa3f50430993fe06453e4294064098119e46
Arthur <oliveirarthur197@hotmail.com>
2024-08-07 14:32:37 -0300

    mudanca no gitignore

--- cypress.config.js
+++ cypress.config.js
@@ -1,10 +1,12 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  projectId: "ne6pm5",
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
     baseUrl: 'http://lojaebac.ebaconline.art.br/',
+    video: true,
   },
 });
